ee54eb2e8f577b7d707727d261dbcad245d3fa00
Jeonghyeon Kim <powergee101@gmail.com>
2023-04-27 22:25:18 +0900

    Remove `cfg-if` from build-dependencies

--- build.rs
+++ build.rs
@@ -1,65 +1,57 @@
-#![feature(rustc_private)]
-#[macro_use]
-extern crate cfg_if;
+// Building custom barrier library must be conducted
+// only in MacOS-based systems.
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+fn main() {
+    extern crate bindgen;
+    extern crate cc;
 
-cfg_if! {
-    // Building custom barrier library must be conducted
-    // only in MacOS-based systems.
-    if #[cfg(any(target_os = "macos", target_os = "ios"))] {
-        extern crate bindgen;
-        extern crate cc;
+    use bindgen::CargoCallbacks;
+    use std::env;
+    use std::path::PathBuf;
 
-        use std::env;
-        use std::path::PathBuf;
+    let libdir_path = PathBuf::from("apple")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize path");
 
-        use bindgen::CargoCallbacks;
+    let sources_path = libdir_path.join("barrier.c");
+    let headers_path = libdir_path.join("barrier.h");
+    let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
 
-        /// Build heavy barrier C library for Apple environment.
-        fn main() {
-            let libdir_path = PathBuf::from("apple")
-                // Canonicalize the path as `rustc-link-search` requires an absolute
-                // path.
-                .canonicalize()
-                .expect("cannot canonicalize path");
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
 
-            let sources_path = libdir_path.join("barrier.c");
-            let headers_path = libdir_path.join("barrier.h");
-            let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
+    // Tell cargo to tell rustc to link our `barrier` library. Cargo will
+    // automatically know it must look for a `libbarrier.a` file.
+    println!("cargo:rustc-link-lib=barrier");
 
-            // Tell cargo to look for shared libraries in the specified directory
-            println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
+    // Tell cargo to invalidate the built crate whenever the header changes.
+    println!("cargo:rerun-if-changed={}", headers_path_str);
 
-            // Tell cargo to tell rustc to link our `barrier` library. Cargo will
-            // automatically know it must look for a `libbarrier.a` file.
-            println!("cargo:rustc-link-lib=barrier");
+    cc::Build::new()
+        .file(&sources_path)
+        .opt_level(3)
+        .compile("barrier");
 
-            // Tell cargo to invalidate the built crate whenever the header changes.
-            println!("cargo:rerun-if-changed={}", headers_path_str);
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .use_core()
+        .allowlist_function("is_supported")
+        .allowlist_function("flush_process_write_buffers")
+        .header(headers_path_str)
+        .parse_callbacks(Box::new(CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
 
-            cc::Build::new()
-                .file(&sources_path)
-                .opt_level(3)
-                .compile("barrier");
-
-            // The bindgen::Builder is the main entry point
-            // to bindgen, and lets you build up options for
-            // the resulting bindings.
-            let bindings = bindgen::Builder::default()
-                .use_core()
-                .allowlist_function("is_supported")
-                .allowlist_function("flush_process_write_buffers")
-                .header(headers_path_str)
-                .parse_callbacks(Box::new(CargoCallbacks))
-                .generate()
-                .expect("Unable to generate bindings");
-
-            // Write the bindings to the $OUT_DIR/barrier.rs file.
-            let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("barrier.rs");
-            bindings
-                .write_to_file(out_path)
-                .expect("Couldn't write bindings!");
-        }
-    } else {
-        fn main() {}
-    }
+    // Write the bindings to the $OUT_DIR/barrier.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("barrier.rs");
+    bindings
+        .write_to_file(out_path)
+        .expect("Couldn't write bindings!");
 }
+
+#[cfg(not(any(target_os = "macos", target_os = "ios")))]
+fn main() {}
